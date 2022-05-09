dcbf775e4ac3b3b1ba5a9fd4b5b517388ebc5f4d
llbyron31 <82618775+llbyron31@users.noreply.github.com>
2022-05-09 17:47:00 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <h1>Crear una nueva tarea</h1>
+
+    <form @submit.prevent="addNewTask()">
+      <label>Tarea </label>
+      <input type="text" placeholder="Ingresa una tarea nueva" v-model="newTasks">
+      <button type="submit">Crear</button>
+    </form>
+    <h2>Lista</h2>
+    <div>
+      <ul v-for="(task, i) in tasks" :key="i">
+        <li>
+          {{ i+1 }}: {{task}}
+          <button @click="deleteTask(i)">Borrar</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        newTasks: '',
+        tasks: [],
+      };
+    },
+    methods: {
+      addNewTask() {
+        this.tasks.push(this.newTasks)
+        this.newTasks = ''
+      },
+      deleteTask(someTask){
+        this.tasks.splice(someTask, 1)
+      }
+    },
+  }
+</script>
+
+<style>
+*{
+  text-align: center;
+}
+</style>
